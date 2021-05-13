e9e9b56350128118c6a40a4812c6e30e1b133eb1
Selvandhan Manivasagam <selvandhan.manivasagam@manifesto.co.uk>
2021-05-13 09:19:42 +0100

    StoreBlogCommentBehavior class refactoring

--- site/webapp/src/main/java/uk/nhs/hee/web/eform/behaviors/StoreBlogCommentBehavior.java
+++ site/webapp/src/main/java/uk/nhs/hee/web/eform/behaviors/StoreBlogCommentBehavior.java
@@ -13,7 +13,6 @@ import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
 import org.hippoecm.hst.core.request.ComponentConfiguration;
 import org.hippoecm.repository.api.HippoWorkspace;
-import org.hippoecm.repository.api.WorkflowException;
 import org.hippoecm.repository.api.WorkflowManager;
 import org.onehippo.repository.documentworkflow.DocumentWorkflow;
 import org.slf4j.Logger;
@@ -24,54 +23,107 @@ import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import java.lang.reflect.InvocationTargetException;
-import java.rmi.RemoteException;
 import java.util.Calendar;
 import java.util.Map;
 
+/**
+ * Class that adds behaviour to perform actions post successful blog comment form validation.
+ *
+ * <p>This class essentially extracts the comment submitted by the site visitor and
+ * adds it ({@code hee:blogComment}) to the corresponding blog post ({@code hee:blogPost}) document.</p>
+ */
 public class StoreBlogCommentBehavior implements OnValidationSuccessBehavior {
     private static final Logger LOGGER = LoggerFactory.getLogger(StoreBlogCommentBehavior.class);
 
     @Override
-    public void onValidationSuccess(HstRequest request, HstResponse response, ComponentConfiguration config, FormBean bean, Form form, FormMap map) {
+    public void onValidationSuccess(
+            final HstRequest request,
+            final HstResponse response,
+            final ComponentConfiguration config,
+            final FormBean bean,
+            final Form form,
+            final FormMap map) {
         try {
-            // In the case of archived blog posts wherein the hst:relativecontentpath is set
-            // in the (non-workspace) sitemap
-            String documentPath = request.getRequestContext().getResolvedSiteMapItem().getRelativeContentPath();
-
-            if (documentPath == null) {
-                // In the case of blog post pages created via channel i.e. for blog post pages
-                // exists in the workspace
-                final HstComponentConfiguration componentConfiguration =
-                        (HstComponentConfiguration) config.getClass().getMethod("getComponentConfiguration").invoke(config);
-                final HstComponentConfiguration blogConfiguration =
-                        componentConfiguration.getParent().getParent().getChildByName("blog").getChildByName("blogpost");
-                documentPath = blogConfiguration.getParameter("document");
-            }
+            final String documentPath = getBlogDocumentPath(request, config);
             if (!Strings.isNullOrEmpty(documentPath)) {
-                final HippoBean root = request.getRequestContext().getSiteContentBaseBean();
-                Session session = FormUtils.getWritableSession();
-                WorkflowManager workflowManager = ((HippoWorkspace) session.getWorkspace()).getWorkflowManager();
-                BlogPost blogPost = root.getBean(documentPath);
-                DocumentWorkflow documentWorkflow = (DocumentWorkflow) workflowManager.getWorkflow("default", session.getNode(blogPost.getNode().getPath()).getParent());
+                final BlogPost blogPost = getBlogPost(request, documentPath);
+                final Session session = FormUtils.getWritableSession();
+                final WorkflowManager workflowManager = ((HippoWorkspace) session.getWorkspace()).getWorkflowManager();
+                final DocumentWorkflow documentWorkflow =
+                        (DocumentWorkflow) workflowManager.getWorkflow(
+                                "default", session.getNode(blogPost.getNode().getPath()).getParent());
 
-                Node editingNode = documentWorkflow.obtainEditableInstance().getNode(session);
-                Node commentNode = editingNode.addNode("hee:comments", "hee:blogComment");
-                Map<String, FormField> formValues = map.getValue();
-                commentNode.setProperty("hee:message", formValues.get("comment").getValue());
-                commentNode.setProperty("hee:postedDate", Calendar.getInstance());
-                Node authorNode = commentNode.addNode("hee:author", "hee:blogCommentAuthor");
-                authorNode.setProperty("hee:firstName", formValues.get("firstName").getValue());
-                authorNode.setProperty("hee:lastName", formValues.get("lastName").getValue());
-                authorNode.setProperty("hee:email", formValues.get("emailAddress").getValue());
+                final Node editingNode = documentWorkflow.obtainEditableInstance().getNode(session);
+                addBlogCommentNode(editingNode, map);
 
                 session.save();
                 documentWorkflow.commitEditableInstance();
-                if((Boolean)documentWorkflow.hints().get("publish")) {
+                if (Boolean.TRUE.equals(documentWorkflow.hints().get("publish"))) {
                     documentWorkflow.publish();
                 }
             }
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | RepositoryException | WorkflowException | RemoteException e) {
+        } catch (final Exception e) {
             LOGGER.warn("Problem on handling blog comment posting: " + e.getMessage(), e);
         }
     }
+
+    /**
+     * Returns blog post document path for the current request.
+     *
+     * @param request the {@link HstRequest} instance.
+     * @param config  the {@link ComponentConfiguration} instance.
+     * @return the blog post document path for the current request.
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     * @throws NoSuchMethodException
+     */
+    private String getBlogDocumentPath(final HstRequest request, final ComponentConfiguration config)
+            throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        // In the case of archived blog posts wherein hst:relativecontentpath is set
+        // in the (non-workspace) sitemap
+        String documentPath = request.getRequestContext().getResolvedSiteMapItem().getRelativeContentPath();
+
+        if (documentPath == null) {
+            // In the case of blog post pages created via channel i.e. for blog post pages
+            // exists in the workspace
+            final HstComponentConfiguration componentConfiguration =
+                    (HstComponentConfiguration) config.getClass().getMethod("getComponentConfiguration").invoke(config);
+            final HstComponentConfiguration blogConfiguration =
+                    componentConfiguration.getParent().getParent().getChildByName("blog").getChildByName("blogpost");
+            documentPath = blogConfiguration.getParameter("document");
+        }
+        return documentPath;
+    }
+
+    /**
+     * Returns {@link BlogPost} instance for the given {@code documentPath}.
+     *
+     * @param request      the {@link HstRequest} instance.
+     * @param documentPath the document path for the current request.
+     * @return the {@link BlogPost} instance for the given {@code documentPath}.
+     */
+    private BlogPost getBlogPost(final HstRequest request, final String documentPath) {
+        final HippoBean root = request.getRequestContext().getSiteContentBaseBean();
+        return root.getBean(documentPath);
+    }
+
+    /**
+     * Extracts blog comment fields from the form {@code map} and adds a {@code hee:comments} node
+     * to the {@code editingNode}.
+     *
+     * @param editingNode the editing blog post node.
+     * @param map         the blog comment form map
+     * @throws RepositoryException thrown when an error occurs during adding blog comment node to the editing node instance.
+     */
+    private void addBlogCommentNode(final Node editingNode, final FormMap map) throws RepositoryException {
+        final Node commentNode = editingNode.addNode("hee:comments", "hee:blogComment");
+        final Map<String, FormField> formValues = map.getValue();
+        commentNode.setProperty("hee:message", formValues.get("comment").getValue());
+        commentNode.setProperty("hee:postedDate", Calendar.getInstance());
+        final Node authorNode = commentNode.addNode("hee:author", "hee:blogCommentAuthor");
+        authorNode.setProperty("hee:firstName", formValues.get("firstName").getValue());
+        authorNode.setProperty("hee:lastName", formValues.get("lastName").getValue());
+        authorNode.setProperty("hee:email", formValues.get("emailAddress").getValue());
+    }
+
 }
