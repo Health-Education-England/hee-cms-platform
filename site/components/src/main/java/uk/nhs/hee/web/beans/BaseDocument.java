23bc4785f503a06245d4716a07d3aa15a79596ec
Raq Khan <rkhan@trycatch.co.uk>
2021-11-07 00:16:33 +0000

    initial commit

--- site/components/src/main/java/uk/nhs/hee/web/beans/BaseDocument.java
+++ site/components/src/main/java/uk/nhs/hee/web/beans/BaseDocument.java
@@ -15,8 +15,4 @@ public class BaseDocument extends HippoDocument {
                 getNode().getProperty(HippoStdPubWfNodeType.HIPPOSTDPUBWF_LAST_MODIFIED_DATE).getDate().getTime(),
                 DateUtils.DD_MMMM_YYYY_PATTERN);
     }
-
-    public String getContentType() throws RepositoryException {
-        return StringUtils.getDocumentTypeDisplayName(this.getNode().getPrimaryNodeType().getName());
-    }
 }
