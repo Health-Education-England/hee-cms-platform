65e9c1a12b4aeeb284145117d220c549f9e5a1a4
Selvandhan Manivasagam <selvandhan.manivasagam@manifesto.co.uk>
2021-11-10 17:24:54 +0000

    NWPS-393: (1) Initial sitemap setup to support index pages (with same name as that of nested folder) as well as support for 10 levels of nesting, (2) Experience page folder and document template queries, (3) Sample layouts and xpages for demo (if required)

--- site/components/src/main/java/uk/nhs/hee/web/components/BlogPostComponent.java
+++ site/components/src/main/java/uk/nhs/hee/web/components/BlogPostComponent.java
@@ -29,8 +29,15 @@ public class BlogPostComponent extends EssentialsDocumentComponent {
     public void doBeforeRender(final HstRequest request, final HstResponse response) {
         super.doBeforeRender(request, response);
 
-        final BlogPost blogPost = request.getModel(REQUEST_ATTR_DOCUMENT);
+        BlogPost blogPost = request.getModel(REQUEST_ATTR_DOCUMENT);
+
+        if (blogPost == null) {
+            blogPost = (BlogPost) request.getRequestContext().getContentBean();
+        }
+
         if (blogPost != null) {
+            request.setModel(REQUEST_ATTR_DOCUMENT, blogPost);
+
             addCategoriesValueListMapToModel(request, blogPost);
 
             addBlogListingPageURLToModel(request);
