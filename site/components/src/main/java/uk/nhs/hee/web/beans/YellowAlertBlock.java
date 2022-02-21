7850fec9d629d5fef01df97edf459a7ac616fd65
sankar-shunmuga <sankar.shunmuga@hee.nhs.uk>
2022-02-21 08:02:10 +0000

    initial commit

--- site/components/src/main/java/uk/nhs/hee/web/beans/YellowAlertBlock.java
+++ site/components/src/main/java/uk/nhs/hee/web/beans/YellowAlertBlock.java
@@ -2,12 +2,12 @@ package uk.nhs.hee.web.beans;
 
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoCompound;
+//import org.hippoecm.hst.content.beans.standard.HippoCompound;
 import org.hippoecm.hst.content.beans.standard.HippoHtml;
 
 @HippoEssentialsGenerated(internalName = "hee:YellowAlertBlock")
 @Node(jcrType = "hee:YellowAlertBlock")
-public class YellowAlertBlock extends HippoCompound {
+public class YellowAlertBlock extends BaseDocument {
     @HippoEssentialsGenerated(internalName = "hee:title")
     public String getTitle() {
         return getSingleProperty("hee:title");
