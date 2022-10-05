590e816ed5a298c67c91ae2e75304699c52c0c3a
Selvandhan Manivasagam <selvandhan.manivasagam@manifesto.co.uk>
2022-10-05 08:18:27 +0100

    Mini-hub component update to include latest updates (e.g. Micor-hero image, table component, etc) from mater

--- site/components/src/main/java/uk/nhs/hee/web/components/MiniHubComponent.java
+++ site/components/src/main/java/uk/nhs/hee/web/components/MiniHubComponent.java
@@ -7,6 +7,7 @@ import org.hippoecm.hst.core.parameters.ParametersInfo;
 import org.onehippo.cms7.essentials.components.EssentialsDocumentComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import uk.nhs.hee.web.beans.Guidance;
 import uk.nhs.hee.web.beans.MiniHub;
 import uk.nhs.hee.web.beans.MinihubSection;
 import uk.nhs.hee.web.components.info.MiniHubComponentInfo;
@@ -16,6 +17,7 @@ import uk.nhs.hee.web.utils.ContentBlocksUtils;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 /**
  * Base component for the MiniHub Page.
@@ -31,36 +33,41 @@ public class MiniHubComponent extends EssentialsDocumentComponent {
         if (document == null) {
             document = (HippoBean) request.getRequestContext().getContentBean();
         }
+
         if (document.getParentBean() != null) {
-            if (document.getParentBean().getChildBeans(MiniHub.class).size() > 0
-                && document.getParentBean().getChildBeans(MinihubSection.class).size() > 0) {
+            if (!document.getParentBean().getChildBeans(MiniHub.class).isEmpty()
+                    && !document.getParentBean().getChildBeans(MinihubSection.class).isEmpty()) {
                 request.setModel(REQUEST_ATTR_DOCUMENT, getMiniHubDocument(document));
 
-                List<MinihubSection> miniHubSections = getMiniHubSections(document);
+                final List<MinihubSection> miniHubSections = getMiniHubSections(document);
                 request.setModel("miniHubSections", miniHubSections);
 
-                MinihubSection currentSection = getCurrentSection(document, miniHubSections);
+                final MinihubSection currentSection = getCurrentSection(document, miniHubSections);
                 request.setModel("currentSection", currentSection);
+
+                if (currentSection == null) {
+                    return;
+                }
+
+                request.setModel("isFirstSection", isFirstSection(document.getName(), miniHubSections));
+
+                // the guidance page contains content blocks that need valueLists to be set on the model
+                final HippoBean currentSectionDocumentBean = currentSection.getDocument();
+
+                if (currentSectionDocumentBean instanceof Guidance) {
+                    final List<HippoBean> pageContentBlocks = ((Guidance) currentSectionDocumentBean).getContentBlocks();
+                    pageContentBlocks.addAll(((Guidance) currentSectionDocumentBean).getRightHandBlocks());
+
+                    final Map<String, Map<String, String>> modelToValueListMap =
+                            ContentBlocksUtils.getValueListMaps(pageContentBlocks);
+                    modelToValueListMap.forEach(request::setModel);
+
+                    request.setAttribute("tableComponentService", new TableComponentService());
+                }
+            } else {
+                LOGGER.warn("Check Minihub document exists, and at least one Minihub section " +
+                        "in the same folder for: {}", document.getPath());
             }
-            else {
-                LOGGER.warn("Check Minihub document exists, and at least one Minihub section in the same folder for: " + document.getPath());
-            }
-
-            /*
-            The following block will need to refactored based on Mini-hub XPage logic
-
-            request.setModel("isFirstPage", guidancePages.get(0).equals(currentGuidance));
-
-            // the guidance page contains content blocks that need valueLists to be set on the model
-            List<HippoBean> pageContentBlocks = currentGuidance.getContentBlocks();
-            pageContentBlocks.addAll(currentGuidance.getRightHandBlocks());
-
-            Map<String, Map<String, String>> modelToValueListMap =
-                    ContentBlocksUtils.getValueListMaps(pageContentBlocks);
-            modelToValueListMap.forEach(request::setModel);
-
-            request.setAttribute("tableComponentService", new TableComponentService());
-            */
         }
     }
 
@@ -70,8 +77,8 @@ public class MiniHubComponent extends EssentialsDocumentComponent {
      * @param document the {@link HippoBean} instance.
      * @return the {@link HippoBean} instance of type hee:MiniHub
      */
-    private HippoBean getMiniHubDocument(HippoBean document) {
-        if(document.getContentType().equals("hee:MiniHub")) {
+    private HippoBean getMiniHubDocument(final HippoBean document) {
+        if ("hee:MiniHub".equals(document.getContentType())) {
             return document;
         }
         return document.getParentBean().getChildBeans(MiniHub.class).get(0);
@@ -83,8 +90,8 @@ public class MiniHubComponent extends EssentialsDocumentComponent {
      * @param document the {@link HippoBean} instance.
      * @return the {@link List <MinihubSection>} instance sorted by Display Name
      */
-    private List<MinihubSection> getMiniHubSections(HippoBean document) {
-        List<MinihubSection> miniHubSections = document.getParentBean().getChildBeans(MinihubSection.class);
+    private List<MinihubSection> getMiniHubSections(final HippoBean document) {
+        final List<MinihubSection> miniHubSections = document.getParentBean().getChildBeans(MinihubSection.class);
         miniHubSections.sort(Comparator.comparing(MinihubSection::getDisplayName));
         return miniHubSections;
     }
@@ -92,22 +99,39 @@ public class MiniHubComponent extends EssentialsDocumentComponent {
     /**
      * Gets the current document of type MinihubSection
      *
-     * @param document the {@link HippoBean} instance.
+     * @param document        the {@link HippoBean} instance.
      * @param miniHubSections the {@link List<MinihubSection>} instance.
      * @return the {@link MinihubSection} instance matching the current position
      */
-    private MinihubSection getCurrentSection(final HippoBean document, List<MinihubSection> miniHubSections) {
+    private MinihubSection getCurrentSection(final HippoBean document, final List<MinihubSection> miniHubSections) {
         MinihubSection currentSection = null;
-        if(document.getContentType().equals("hee:MiniHub")) {
+        if (document.getContentType().equals("hee:MiniHub")) {
             currentSection = miniHubSections.get(0);
-        }
-        else {
-            for (int i=0; i<miniHubSections.size(); i++) {
-                if(miniHubSections.get(i).getName().equalsIgnoreCase(document.getName())){
-                    currentSection = miniHubSections.get(i);
+        } else {
+            for (final MinihubSection miniHubSection : miniHubSections) {
+                if (miniHubSection.getName().equalsIgnoreCase(document.getName())) {
+                    currentSection = miniHubSection;
                 }
             }
         }
         return currentSection;
+    }
+
+    /**
+     * Returns {@code true} if the current section identified by {@code currentSectionName} is the first section
+     * of the Mini-hub. Otherwise, returns {@code false}.
+     *
+     * @param currentSectionName the current section name.
+     * @param miniHubSections    the {@link List<MinihubSection>} of Mini-hub sections.
+     * @return {@code true} if the current section identified by {@code currentSectionName} is the first section
+     * of the Mini-hub. Otherwise, returns {@code false}.
+     */
+    private boolean isFirstSection(final String currentSectionName, final List<MinihubSection> miniHubSections) {
+        final int currentSectionIndex = IntStream.range(0, miniHubSections.size())
+                .filter(i -> miniHubSections.get(i).getName().equals(currentSectionName))
+                .findFirst()
+                .orElse(-1);
+
+        return currentSectionIndex == 0;
     }
 }
