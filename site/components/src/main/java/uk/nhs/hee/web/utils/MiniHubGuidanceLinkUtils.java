1cc3b3ecf61ea7d7ba54aca5b548f43110ba7296
Selvandhan Manivasagam <selvandhan.manivasagam@manifesto.co.uk>
2022-10-08 12:32:29 +0100

    NWPS-809: (1) Added 'pagenotfound' check for associated Mini-hub page URL before using it to construct Mini-hub Guidance page URL, (2) Checks all associated Mini-hub nodes if a valid link is available to generate Mini-hub Guidance URL, (3) Replaced 'requestContext.isChannelManagerPreviewRequest()' with 'requestContext.isPreview()' to handle preview requests from [CMS] Channel Manager as well as channel '/preview' mount.

--- site/components/src/main/java/uk/nhs/hee/web/utils/MiniHubGuidanceLinkUtils.java
+++ site/components/src/main/java/uk/nhs/hee/web/utils/MiniHubGuidanceLinkUtils.java
@@ -61,19 +61,26 @@ public class MiniHubGuidanceLinkUtils {
 
             log.debug("'{}' is a Guidance ({}) document", node.getPath(), GUIDANCE_DOCUMENT_TYPE);
 
-            final Node miniHubNode =
-                    getReferencedMiniHubNode(requestContext, requestContext.getSiteContentBasePath(), node.getIdentifier());
+            final NodeIterator miniHubNodeIterator = getReferencedMiniHubNodes(
+                    requestContext,
+                    requestContext.getSiteContentBasePath(),
+                    node.getIdentifier());
 
-            if (miniHubNode == null) {
-                log.debug("Guidance node with UUID '{}' and path '{}' hasn't been associated to any Mini-hub page(s)",
+            if (miniHubNodeIterator.getSize() == 0) {
+                log.debug("Guidance node with UUID '{}' and path '{}' hasn't been associated " +
+                        "to any Mini-hub (hee:MiniHub) documents", node.getIdentifier(), node.getPath());
+                return null;
+            }
+
+            final HstLink miniHubGuidanceLink =
+                    getFirstAvailableMiniHubLink(miniHubNodeIterator, canonical, node, requestContext, mount);
+
+            if (miniHubGuidanceLink == null) {
+                log.debug("Guidance node with UUID '{}' and path '{}' doesn't have a valid Mini-hub page URL",
                         node.getIdentifier(), node.getPath());
                 return null;
             }
 
-            log.debug("Guidance node with UUID '{}' and path '{}' has been associated to the Mini-hub page = {}",
-                    node.getIdentifier(), node.getPath(), miniHubNode.getPath());
-
-            final HstLink miniHubGuidanceLink = createInternalLink(miniHubNode, canonical, requestContext, mount);
             // Mini-hub Guidance URL format: {mini-hub_URL}/{guidance_node_name}
             final String miniHubGuidanceURL = miniHubGuidanceLink.getPath() + "/" + node.getName();
             log.debug("URL generated for the Guidance node with UUID '{}' and path '{}' " +
@@ -103,24 +110,25 @@ public class MiniHubGuidanceLinkUtils {
     }
 
     /**
-     * Returns the first MiniHub ({@code hee:MiniHub}) document Handle node
-     * to which the given Guidance document ({@code guidanceUUID}) has been associated. Otherwise, returns {@code null}.
+     * Returns a {@link NodeIterator} instance containing ({@code hee:MiniHub}) document Handle nodes
+     * to which the given Guidance document ({@code guidanceUUID}) has been associated.
      *
      * @param requestContext the {@link HstRequestContext} instance.
      * @param contentPath    the (channel) content path under which the referenced MiniHub document
      *                       needs to be searched for.
      * @param guidanceUUID   the UUID of the Guidance Handle node.
-     * @return the first MiniHub ({@code hee:MiniHub}) document Handle node
-     * to which the given Guidance document ({@code guidanceUUID}) has been associated. Otherwise, returns {@code null}.
+     * @return a {@link NodeIterator} instance containing ({@code hee:MiniHub}) document Handle nodes
+     * to which the given Guidance document ({@code guidanceUUID}) has been associated.
      * @throws RepositoryException thrown when an error occurs while searching for referenced MiniHub in the repository.
      */
-    private static Node getReferencedMiniHubNode(
+    private static NodeIterator getReferencedMiniHubNodes(
             final HstRequestContext requestContext,
             final String contentPath,
-            final String guidanceUUID) throws RepositoryException {
+            final String guidanceUUID
+    ) throws RepositoryException {
         final String formattedReferencedMiniHubNodeFinderQuery = String.format(REFERENCED_MINI_HUB_NODE_FINDER_QUERY,
                 contentPath,
-                requestContext.isChannelManagerPreviewRequest() ? AVAILABILITY_PREVIEW : AVAILABILITY_LIVE,
+                requestContext.isPreview() ? AVAILABILITY_PREVIEW : AVAILABILITY_LIVE,
                 guidanceUUID);
 
         log.debug("Formatted referenced Mini-hub (hee:MiniHub) node finder query = {}",
@@ -132,23 +140,79 @@ public class MiniHubGuidanceLinkUtils {
                         Query.XPATH
                 );
         final QueryResult results = referencedMiniHubNodeFinderQuery.execute();
-        final NodeIterator nodeIterator = results.getNodes();
 
-        if (nodeIterator.hasNext()) {
-            return nodeIterator.nextNode();
+        return results.getNodes();
+    }
+
+    /**
+     * Returns an {@link HstLink} instance for the first Mini-hub node (in the {@code miniHubNodeIterator})
+     * to which an associated sitemap is available.
+     *
+     * @param miniHubNodeIterator the {@link NodeIterator} instance containing Mini-hub nodes which needs to checked
+     *                            if an associated sitemap is available.
+     * @param canonical           boolean indicating whether canonical link should be returned or not.
+     * @param guidanceNode        the Guidance {@link Node} instance (which has been associated
+     *                            to the Mini-hub nodes available in {@code miniHubNodeIterator})
+     * @param requestContext      the context for the current request.
+     * @param targetMount         the target {@link Mount} instance.
+     * @return an {@link HstLink} instance for the first Mini-hub node (in the {@code miniHubNodeIterator})
+     * to which an associated sitemap is available.
+     * @throws RepositoryException thrown when an error occurs while finding the first available Mini-hub {@link HstLink}
+     *                             in the given {@code miniHubNodeIterator}.
+     */
+    private static HstLink getFirstAvailableMiniHubLink(
+            final NodeIterator miniHubNodeIterator,
+            final boolean canonical,
+            final Node guidanceNode,
+            final HstRequestContext requestContext,
+            final Mount targetMount
+    ) throws RepositoryException {
+
+        while (miniHubNodeIterator.hasNext()) {
+            final Node miniHubNode = miniHubNodeIterator.nextNode();
+            log.debug("Guidance node with UUID '{}' and path '{}' has been associated to the Mini-hub page = {}",
+                    guidanceNode.getIdentifier(), guidanceNode.getPath(), miniHubNode.getPath());
+
+            final HstLink miniHubGuidanceLink = createInternalLink(miniHubNode, canonical, requestContext, targetMount);
+            if (miniHubGuidanceLink == null || isPageNotFound(miniHubGuidanceLink, requestContext, targetMount)) {
+                log.debug("Mini-hub node with UUID '{}' and path '{}' doesn't have any sitemap associated with it " +
+                                "and so couldn't generate URL for it. Skipping and continuing...",
+                        miniHubNode.getIdentifier(), miniHubNode.getPath());
+                continue;
+            }
+
+            return miniHubGuidanceLink;
         }
 
         return null;
     }
 
     /**
+     * Returns {@code true} if the given Mini-hub link is a {@code pagenotfound} URL. Otherwise, returns {@code false}.
+     *
+     * @param miniHubLink    the Mini-hub {@link HstLink} instance.
+     * @param requestContext the context for the current request.
+     * @param targetMount    the target {@link Mount} instance.
+     * @return {@code true} if the given Mini-hub link is a {@code pagenotfound} URL. Otherwise, returns {@code false}.
+     */
+    private static boolean isPageNotFound(
+            final HstLink miniHubLink,
+            final HstRequestContext requestContext,
+            final Mount targetMount) {
+        final String pageNotFoundURL = HstUtils.getPageNotFoundURL(requestContext, targetMount, false);
+
+        return miniHubLink.toUrlForm(requestContext, false).equals(pageNotFoundURL);
+    }
+
+    /**
      * Create an HstLink to a referenced node in rich text.
      *
      * @param referencedNode the node to create a link to
-     * @param requestContext the context for the current request
-     * @param targetMount    mount that the link by preference points to. may be null. If not null, a link for a different
-     *                       {@link Mount} might be returned
-     * @return link to the referenced node and optional target mount. target mount is ignored in case links must be canonical.
+     * @param requestContext the context for the current request.
+     * @param targetMount    mount that the link by preference points to. may be {@code null}.
+     *                       If not {@code null}, a link for a different {@link Mount} might be returned.
+     * @return link to the referenced node and optional target mount. Target mount is ignored
+     * in case links must be canonical.
      * @throws RepositoryException if no path can be retrieved from the referencedNode
      */
     protected static HstLink createInternalLink(
@@ -159,8 +223,9 @@ public class MiniHubGuidanceLinkUtils {
     ) throws RepositoryException {
         if (canonical) {
             if (targetMount != null) {
-                log.info("TargetMount is defined to create a link for, but target mount is ignored in case a canonical link is " +
-                                "requested. Ignoring target mount '{}' but instead return canonical link for nodepath '{}'.",
+                log.info("TargetMount is defined to create a link for, but target mount is ignored " +
+                                "in case a canonical link is requested. Ignoring target mount '{}' " +
+                                "but instead return canonical link for nodepath '{}'.",
                         targetMount, referencedNode.getPath());
             }
             return requestContext.getHstLinkCreator().createCanonical(referencedNode, requestContext);
