6863f5d62b68565eae8a6515668ec7295cc242a4
Selvandhan Manivasagam <selvandhan.manivasagam@manifesto.co.uk>
2022-12-06 09:16:44 +0000

    NWPS-1136: Removing content block processing logic from PublicationLandingPageComponent as we don't have one in the content model/document type

--- site/components/src/main/java/uk/nhs/hee/web/components/PublicationLandingPageComponent.java
+++ site/components/src/main/java/uk/nhs/hee/web/components/PublicationLandingPageComponent.java
@@ -1,39 +1,12 @@
 package uk.nhs.hee.web.components;
 
-import org.hippoecm.hst.content.beans.standard.HippoBean;
-import org.hippoecm.hst.core.component.HstRequest;
-import org.hippoecm.hst.core.component.HstResponse;
 import org.hippoecm.hst.core.parameters.ParametersInfo;
 import org.onehippo.cms7.essentials.components.EssentialsDocumentComponent;
-import uk.nhs.hee.web.beans.PublicationLandingPage;
 import uk.nhs.hee.web.components.info.PublicationLandingPageComponentInfo;
-import uk.nhs.hee.web.services.TableComponentService;
-import uk.nhs.hee.web.utils.ContentBlocksUtils;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * Component class for {@code hee:publicationLandingPage} document type pages.
  */
 @ParametersInfo(type = PublicationLandingPageComponentInfo.class)
 public class PublicationLandingPageComponent extends EssentialsDocumentComponent {
-
-    @Override
-    public void doBeforeRender(final HstRequest request, final HstResponse response) {
-        super.doBeforeRender(request, response);
-
-        final PublicationLandingPage publicationLandingPage = request.getModel(REQUEST_ATTR_DOCUMENT);
-        if (publicationLandingPage != null) {
-            // the page content blocks needs valueLists to be set on the model
-            final List<HippoBean> pageContentBlocks = publicationLandingPage.getContentBlocks();
-
-            final Map<String, Map<String, String>> modelToValueListMap =
-                    ContentBlocksUtils.getValueListMaps(pageContentBlocks);
-            modelToValueListMap.forEach(request::setModel);
-
-            request.setAttribute("tableComponentService", new TableComponentService());
-        }
-    }
-
 }
