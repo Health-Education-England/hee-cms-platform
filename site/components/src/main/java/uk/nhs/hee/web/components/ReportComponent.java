e143c9192a0c578d743afee1c0c2301fae2f6c17
FranciscoRuizHEE <francisco.ruiz@hee.nhs.uk>
2022-12-12 10:06:24 +0000

    Right hand public info box

--- site/components/src/main/java/uk/nhs/hee/web/components/ReportComponent.java
+++ site/components/src/main/java/uk/nhs/hee/web/components/ReportComponent.java
@@ -5,13 +5,17 @@ import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
 import org.hippoecm.hst.core.parameters.ParametersInfo;
 import org.onehippo.cms7.essentials.components.EssentialsDocumentComponent;
+import uk.nhs.hee.web.beans.PublicationLandingPage;
 import uk.nhs.hee.web.beans.Report;
 import uk.nhs.hee.web.components.info.ReportComponentInfo;
 import uk.nhs.hee.web.services.TableComponentService;
 import uk.nhs.hee.web.utils.ContentBlocksUtils;
+import uk.nhs.hee.web.utils.ReportAndPublicationUtils;
 
 import java.util.List;
 import java.util.Map;
+
+import javax.jcr.RepositoryException;
 
 /**
  * Component class for {@code hee:report} document type pages.
@@ -33,7 +37,14 @@ public class ReportComponent extends EssentialsDocumentComponent {
             modelToValueListMap.forEach(request::setModel);
 
             request.setAttribute("tableComponentService", new TableComponentService());
+            try {
+                ReportAndPublicationUtils reportUtils = new ReportAndPublicationUtils();
+
+                PublicationLandingPage landingPageParent = reportUtils.findMyParent(reportPage, request.getRequestContext());
+                request.setModel("landingPage", landingPageParent);
+            } catch (RepositoryException e) {
+                e.printStackTrace();
+            }
         }
     }
-
 }
