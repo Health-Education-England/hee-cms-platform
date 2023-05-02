8f2acaeec643bab5355b834f3bc9477d1c30ff0b
Ian Pearce <ian.pearce@manifesto.co.uk>
2023-05-02 20:21:15 +0100

    [NWPS-1289] Enforce default description for images

--- cms/src/main/java/uk/nhs/hee/web/repository/gallery/impl/HEEGalleryWorkflowImpl.java
+++ cms/src/main/java/uk/nhs/hee/web/repository/gallery/impl/HEEGalleryWorkflowImpl.java
@@ -1,19 +1,18 @@
 package uk.nhs.hee.web.repository.gallery.impl;
 
 import org.hippoecm.repository.api.Document;
-import org.hippoecm.repository.api.NodeNameCodec;
 import org.hippoecm.repository.api.WorkflowException;
 import org.hippoecm.repository.gallery.impl.GalleryWorkflowImpl;
 
 import java.rmi.RemoteException;
-import java.text.MessageFormat;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
 public class HEEGalleryWorkflowImpl extends GalleryWorkflowImpl {
-    public static final String HIPPOGALLERY_DESCRIPTION = "hippogallery:description";
+    private static final String PLEASE_ENTER_A_CAPTION = "Please enter a caption";
+    private static final String HIPPOGALLERY_DESCRIPTION = "hippogallery:description";
 
     private Session myRootSession;
 
@@ -26,7 +25,7 @@ public class HEEGalleryWorkflowImpl extends GalleryWorkflowImpl {
     public Document createGalleryItem(String nodeName, String type, String hippoGalleryImageSetFileName) throws RemoteException, RepositoryException, WorkflowException {
         Document document = super.createGalleryItem(nodeName, type, hippoGalleryImageSetFileName);
         Node thisNode = document.getNode(myRootSession);
-        thisNode.setProperty(HIPPOGALLERY_DESCRIPTION, hippoGalleryImageSetFileName);
+        thisNode.setProperty(HIPPOGALLERY_DESCRIPTION, PLEASE_ENTER_A_CAPTION);
         return document;
     }
 }
