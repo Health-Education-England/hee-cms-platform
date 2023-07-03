e46b64c785fb8c4932e7ba03c3a87e809c393130
Selvandhan Manivasagam <selvandhan.manivasagam@tpximpact.com>
2023-07-03 18:20:52 +0530

    [NWPS-1581] Html (hippostd:html) validator build for content block

--- cms/src/main/java/uk/nhs/hee/web/validation/validator/ContentCardContentBlockValidator.java
+++ cms/src/main/java/uk/nhs/hee/web/validation/validator/ContentCardContentBlockValidator.java
@@ -16,7 +16,7 @@ import javax.jcr.RepositoryException;
 import java.util.Optional;
 
 /**
- * Content card (compound type) ({@code hee:contentCards}) validator when used in a content block.
+ * Content card (compound type) ({@code hee:contentCards}) validator when used as a content block.
  *
  * <p>validates if the editor has provided the header ({@code hee:header}) as well as the navigation/content card
  * documents ({@code hee:cards}) for content/navigation cards {@code hee:contentCards} content block.</p>
--- cms/src/main/java/uk/nhs/hee/web/validation/validator/RichTextContentBlockValidator.java
+++ cms/src/main/java/uk/nhs/hee/web/validation/validator/RichTextContentBlockValidator.java
@@ -1,8 +1,8 @@
 package uk.nhs.hee.web.validation.validator;
 
+import org.hippoecm.frontend.plugins.richtext.validation.RequiredRichTextValidator;
+import org.hippoecm.repository.HippoStdNodeType;
 import org.hippoecm.repository.util.JcrUtils;
-import org.onehippo.addon.frontend.gallerypicker.GalleryPickerNodeType;
-import org.onehippo.addon.frontend.gallerypicker.validator.RequiredImageLinkValidator;
 import org.onehippo.cms.services.validation.api.ValidationContext;
 import org.onehippo.cms.services.validation.api.Violation;
 import org.slf4j.Logger;
@@ -13,25 +13,24 @@ import javax.jcr.RepositoryException;
 import java.util.Optional;
 
 /**
- * <p>An extension of out of the box {@link RequiredImageLinkValidator} which performs required validation
- * for the Image link (compound type) ({@code hippogallerypicker:imagelink}) when used as a content block.</p>
+ * <p>An extension of out of the box {@link RequiredRichTextValidator} which performs required validation
+ * for the html (compound type) ({@code hippostd:html}) when used as a content block.</p>
  *
- * <p>validates if the editor has selected/chosen an image
- * for the Image link (compound type) ({@code hippogallerypicker:imagelink}) content block.</p>
+ * <p>validates if the editor has provided some text
+ * for the html (compound type) ({@code hippostd:html}) content block.</p>
  */
-public class ImageLinkContentBlockValidator extends RequiredImageLinkValidator {
-
+public class RichTextContentBlockValidator extends RequiredRichTextValidator {
     // Logger
-    private static final Logger LOGGER = LoggerFactory.getLogger(ImageLinkContentBlockValidator.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RichTextContentBlockValidator.class);
 
     @Override
     public Optional<Violation> validate(final ValidationContext context, final Node node) {
         try {
-            if (!node.isNodeType(GalleryPickerNodeType.NT_IMAGE_LINK)) {
+            if (!node.isNodeType(HippoStdNodeType.NT_HTML)) {
                 return Optional.empty();
             }
 
-            return super.validate(context, node);
+            return super.checkNode(context, node);
         } catch (final RepositoryException e) {
             LOGGER.warn(
                     "Caught '{}' error while reading properties of the node '{}'",
@@ -42,4 +41,5 @@ public class ImageLinkContentBlockValidator extends RequiredImageLinkValidator {
 
         return Optional.empty();
     }
+
 }
