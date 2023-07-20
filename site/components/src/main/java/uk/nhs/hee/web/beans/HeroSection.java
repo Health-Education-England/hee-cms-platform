c0e540d6e926b962d15fae88c3437740160652ee
Selvandhan Manivasagam <selvandhan.manivasagam@tpximpact.com>
2023-07-20 16:49:39 +0530

    [NWPS-1374] Removing the legacy/unused 'hee:HeroSection' compound type and its associated updates

--- site/components/src/main/java/uk/nhs/hee/web/beans/HeroSection.java
+++ site/components/src/main/java/uk/nhs/hee/web/beans/HeroSection.java
@@ -1,19 +1,10 @@
 package uk.nhs.hee.web.beans;
 
-import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 import org.hippoecm.hst.content.beans.Node;
 import org.hippoecm.hst.content.beans.standard.HippoCompound;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 
-@HippoEssentialsGenerated(internalName = "hee:HeroSection")
+@HippoEssentialsGenerated(internalName = "hee:HeroSection", allowModifications = false)
 @Node(jcrType = "hee:HeroSection")
 public class HeroSection extends HippoCompound {
-    @HippoEssentialsGenerated(internalName = "hee:header")
-    public String getHeader() {
-        return getSingleProperty("hee:header");
-    }
-
-    @HippoEssentialsGenerated(internalName = "hee:text")
-    public String getText() {
-        return getSingleProperty("hee:text");
-    }
 }
