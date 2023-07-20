06db5a71f1995c0e02507684eb8925d47acbe801
Selvandhan Manivasagam <selvandhan.manivasagam@tpximpact.com>
2023-07-20 18:19:04 +0530

    [NWPS-1374] Removal of legacy/unused 'hee:AfterForm' document type and its associated updates

--- site/components/src/main/java/uk/nhs/hee/web/beans/AfterForm.java
+++ site/components/src/main/java/uk/nhs/hee/web/beans/AfterForm.java
@@ -1,20 +1,9 @@
 package uk.nhs.hee.web.beans;
 
-import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoHtml;
-import uk.nhs.hee.web.beans.Link;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 
-@HippoEssentialsGenerated(internalName = "hee:AfterForm")
+@HippoEssentialsGenerated(internalName = "hee:AfterForm", allowModifications = false)
 @Node(jcrType = "hee:AfterForm")
 public class AfterForm extends BaseDocument {
-    @HippoEssentialsGenerated(internalName = "hee:mainBody")
-    public HippoHtml getMainBody() {
-        return getHippoHtml("hee:mainBody");
-    }
-
-    @HippoEssentialsGenerated(internalName = "hee:backLink")
-    public Link getBackLink() {
-        return getBean("hee:backLink", Link.class);
-    }
 }
