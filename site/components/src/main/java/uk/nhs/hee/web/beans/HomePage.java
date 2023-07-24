ec9fc4b2642b4b8293ef0d69495492540f7af7ab
Selvandhan Manivasagam <selvandhan.manivasagam@tpximpact.com>
2023-07-24 15:15:27 +0530

    [NWPS-1655] Adding the missing "@HippoEssentialsGenerated" annotation for the content block bean fields to avoid "TODO: Beanwriter: Failed to create getter for node type: hippo:compound" error being generated by bean writer tool

--- site/components/src/main/java/uk/nhs/hee/web/beans/HomePage.java
+++ site/components/src/main/java/uk/nhs/hee/web/beans/HomePage.java
@@ -3,14 +3,13 @@ package uk.nhs.hee.web.beans;
 import org.hippoecm.hst.content.beans.Node;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+
 import java.util.List;
 
-/** 
- * TODO: Beanwriter: Failed to create getter for node type: hippo:compound
- */
 @HippoEssentialsGenerated(internalName = "hee:HomePage")
 @Node(jcrType = "hee:HomePage")
 public class HomePage extends BaseDocument {
+    @HippoEssentialsGenerated(internalName = "hee:contentBlocks")
     public <T extends HippoBean> List<T> getContentBlocks() {
         return getChildBeansByName("hee:contentBlocks");
     }
