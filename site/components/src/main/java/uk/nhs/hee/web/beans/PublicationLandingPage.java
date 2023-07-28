c99a1f044ff6ad81b5c1e6d0f8f8381c6ff70ec0
smanivasag <35143542+smanivasag@users.noreply.github.com>
2023-07-28 12:20:30 +0100

    [NWPS-1655] Adding the missing "@HippoEssentialsGenerated" annotation for the content block bean fields to avoid "TODO: Beanwriter: Failed to create getter for node type: hippo:compound" error being generated by bean writer tool (#476)
    Co-authored-by: sankar-shunmuga <95379560+sankar-shunmuga@users.noreply.github.com>

--- site/components/src/main/java/uk/nhs/hee/web/beans/PublicationLandingPage.java
+++ site/components/src/main/java/uk/nhs/hee/web/beans/PublicationLandingPage.java
@@ -1,14 +1,11 @@
 package uk.nhs.hee.web.beans;
 
-import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 import org.hippoecm.hst.content.beans.Node;
-import java.util.Calendar;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import java.util.Calendar;
 import java.util.List;
 
-/** 
- * TODO: Beanwriter: Failed to create getter for node type: hippo:compound
- */
 @HippoEssentialsGenerated(internalName = "hee:publicationLandingPage")
 @Node(jcrType = "hee:publicationLandingPage")
 public class PublicationLandingPage extends BaseDocument {
@@ -85,6 +82,15 @@ public class PublicationLandingPage extends BaseDocument {
     public String getOtherFormatsEmail() {
         return getSingleProperty("hee:otherFormatsEmail");
     }
+
+    @HippoEssentialsGenerated(internalName = "hee:assetVersionsContent")
+    public List<AssetResource> getAssetVersionsContent() {
+        return getChildBeansByName("hee:assetVersionsContent", AssetResource.class);
+    }
+
+    @HippoEssentialsGenerated(internalName = "hee:languageVersionsContent")
+    public List<AssetResource> getLanguageVersionsContent() {
+        return getChildBeansByName("hee:languageVersionsContent", AssetResource.class);
 
     @HippoEssentialsGenerated(internalName = "hee:featuredContent")
     public FeaturedContentReference getFeaturedContent() {
--- site/components/src/main/java/uk/nhs/hee/web/beans/Report.java
+++ site/components/src/main/java/uk/nhs/hee/web/beans/Report.java
@@ -3,13 +3,10 @@ package uk.nhs.hee.web.beans;
 import org.hippoecm.hst.content.beans.Node;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+
 import java.util.Calendar;
 import java.util.List;
-import uk.nhs.hee.web.beans.FeaturedContentReference;
 
-/** 
- * TODO: Beanwriter: Failed to create getter for node type: hippo:compound
- */
 @HippoEssentialsGenerated(internalName = "hee:report")
 @Node(jcrType = "hee:report")
 public class Report extends BaseDocument {
@@ -23,6 +20,7 @@ public class Report extends BaseDocument {
         return getSingleProperty("hee:summary");
     }
 
+    @HippoEssentialsGenerated(internalName = "hee:contentBlocks")
     public <T extends HippoBean> List<T> getContentBlocks() {
         return getChildBeansByName("hee:contentBlocks");
     }
