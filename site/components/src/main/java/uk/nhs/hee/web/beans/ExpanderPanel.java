a340c30590aa1d8aaaa48487e8d68612ad8d78bd
FranciscoRuizHEE <francisco.ruiz@hee.nhs.uk>
2023-09-01 11:24:04 +0100

    NWPS-1690-be-nested-content-block-expander-group
    Add content blocks into the Expander Panel for content editors to be able to add any number of blocks in each expander panel

--- site/components/src/main/java/uk/nhs/hee/web/beans/ExpanderPanel.java
+++ site/components/src/main/java/uk/nhs/hee/web/beans/ExpanderPanel.java
@@ -1,9 +1,12 @@
 package uk.nhs.hee.web.beans;
 
+import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 import org.hippoecm.hst.content.beans.Node;
 import org.hippoecm.hst.content.beans.standard.HippoCompound;
 import org.hippoecm.hst.content.beans.standard.HippoHtml;
+
+import java.util.List;
 
 @HippoEssentialsGenerated(internalName = "hee:expanderPanel")
 @Node(jcrType = "hee:expanderPanel")
@@ -17,4 +20,10 @@ public class ExpanderPanel extends HippoCompound {
     public HippoHtml getExpanderDetails() {
         return getHippoHtml("hee:expanderDetails");
     }
+
+    @HippoEssentialsGenerated(internalName = "hee:contentBlocks")
+    public <T extends HippoBean> List<T> getContentBlocks() {
+        return getChildBeansByName("hee:contentBlocks");
+    }
+
 }
