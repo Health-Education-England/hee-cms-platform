2562331cccbb8988ec208f2b0903e36a2e58edfc
FranciscoRuizHEE <francisco.ruiz@hee.nhs.uk>
2023-09-07 14:55:31 +0100

    nwps 1657 be nested content block details
    Add method for hee:contentBlocks field

--- site/components/src/main/java/uk/nhs/hee/web/beans/Details.java
+++ site/components/src/main/java/uk/nhs/hee/web/beans/Details.java
@@ -1,8 +1,11 @@
 package uk.nhs.hee.web.beans;
 
+import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 import org.hippoecm.hst.content.beans.Node;
 import org.hippoecm.hst.content.beans.standard.HippoHtml;
+
+import java.util.List;
 
 @HippoEssentialsGenerated(internalName = "hee:details")
 @Node(jcrType = "hee:details")
@@ -16,4 +19,10 @@ public class Details extends BaseDocument {
     public HippoHtml getRichStatement() {
         return getHippoHtml("hee:richStatement");
     }
+
+    @HippoEssentialsGenerated(internalName = "hee:contentBlocks")
+    public <T extends HippoBean> List<T> getContentBlocks() {
+        return getChildBeansByName("hee:contentBlocks");
+    }
+
 }
