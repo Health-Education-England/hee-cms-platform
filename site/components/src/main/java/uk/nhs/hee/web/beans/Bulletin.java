0bc609a2abf112b652ab5121590a2f826b8613d2
smanivasag <selvandhan.manivasagam@tpximpact.com>
2024-01-11 09:17:28 +0000

    [NWPS-1766] (1) Removing unused 'uk.nhs.hee.web.beans.RichTextWithoutHeadingOneAndTwo' content bean, (2) Adding an empty content bean for 'hee:bulletin' document type with 'allowModifications=false' to avoid regenerating it during every Beanwriter essential tool usage.

---
+++ site/components/src/main/java/uk/nhs/hee/web/beans/Bulletin.java
@@ -0,0 +1,9 @@
+package uk.nhs.hee.web.beans;
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.Node;
+
+@HippoEssentialsGenerated(internalName = "hee:bulletin", allowModifications = false)
+@Node(jcrType = "hee:bulletin")
+public class Bulletin extends BaseDocument {
+}
